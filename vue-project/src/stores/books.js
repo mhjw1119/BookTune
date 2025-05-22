19d0fa2d2dd8011fbc7fe6c9d6548b48dae841bb
YH-edenbro <hyun96227@gmail.com>
2025-05-22 17:55:24 +0900

    book.js

--- vue-project/src/stores/books.js
+++ vue-project/src/stores/books.js
@@ -6,12 +6,32 @@ export const useBookStore = defineStore('book', () => {
   const books = ref([])
   const API_URL = 'http://127.0.0.1:8000'
 
+  // axios 인스턴스 생성
+  const axiosInstance = axios.create({
+    baseURL: API_URL,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  // 요청 인터셉터 추가
+  axiosInstance.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('access_token')
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+      }
+      return config
+    },
+    (error) => {
+      return Promise.reject(error)
+    }
+  )
+
   const getBooks = async function () {
     try {
-      const response = await axios({
-        method: 'get',
-        url: `${API_URL}/api/books/`
-      })
+      // 토큰 없이도 요청 가능하도록 일반 axios 사용
+      const response = await axios.get(`${API_URL}/api/books/`)
       books.value = response.data
       return response.data
     } catch (error) {
